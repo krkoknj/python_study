e16de77029eecffd73ec0f3dbeb5eac69b1f9af3
¤knj <krkoknj@gmail.com>
2022-11-14 00:29:59 +0900

    day2 study

---
+++ day_2/day-2-end.py
@@ -0,0 +1,13 @@
+print("Hello"[0])
+
+num = 123_456_789
+
+print(num)
+
+num_char = len(input("What is your name?"))
+new_num_char = str(num_char)
+print("Your name has " + new_num_char + " characters.");
+
+print(type(num_char))
+
+
